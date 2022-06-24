73777b5e573802540020bbcdee9d7f3600b16356
filipeoxs <filipeoxs@gmail.com>
2022-06-24 09:01:31 -0300

    publicando o keys.py

---
+++ website/keys.py
@@ -0,0 +1,10 @@
+KEY_DB = '764c4fec3bd1d103ea828523d19d33304781968083eb6804d75d64ec82156e62'
+
+USER_DB = 'srlhssghumkjtj'
+
+HOST_DB = 'ec2-44-197-128-108.compute-1.amazonaws.com'
+
+PORT_DB = '5432'
+
+KEY_DJANGO = 'django-insecure-ub_&k48p^$1^cl#=u_w##j$g&2t4zqz&_0bb56q5d4h%rb5!%j'
+
